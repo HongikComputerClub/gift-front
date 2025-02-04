028802edfde44d09a05700a28ba64719747519d3
YangYoonseo <yys0847@naver.com>
2025-02-04 14:01:28 +0900

    버튼 주석 추가

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -2,7 +2,7 @@ import "./Button.css";
 
 const Button = ({ text, type, onClick }) => {
   return (
-    // 배경색에 따라 type은 black, white 2개로 분리
+    // 배경색에 따라 type은 black, white, gray 3개로 분리
     <button onClick={onClick} className={`Button Button_${type}`}>
       {text}
     </button>
